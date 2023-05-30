fd483f1383df0f7452922bb08dec5499b9bc757a
Matthew Baer <matt.baer77@gmail.com>
2023-05-29 23:36:27 -0400

    initial commit - corrected

---
+++ step2.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+function cat(path) {
+
+    fs.readFile(path, 'utf8', (err, data) => {
+
+        if (err) {
+            console.log(`Error reading ${path}`, err)
+            process.kill(1)
+        }
+
+        console.log(data)
+
+    })
+
+}
+
+async function webCat(url) {
+
+    try {
+        let resp = await axios.get(url);
+        console.log(resp.data);
+    }
+
+    catch (err) {
+
+        console.log(`Error finding ${url}`)
+        process.kill(1)
+
+    }
+
+}
+
+let path = process.argv[2];
+
+if (path.slice(0,4) === 'http') {
+    webCat(path);
+}
+
+else {
+    cat(path)
+}
+
+// cat(process.argv[2])
